6da801c198536aa1c40f4384e86ba9eed82197b4
Nobkun16 <119097285+Nobkun16@users.noreply.github.com>
2025-08-10 17:15:49 +0800

    footer responsive

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -17,9 +17,9 @@ function Footer(){
                     <tr>
                         <td>John Philip Baylon</td>
                         <td className="icon-container"> 
-                            <a href="https://www.facebook.com/johnphilip.baylon.5"><FaFacebook size={30}></FaFacebook></a>
-                            <a href="mailto:johnphilipbaylon3@gmail.com"> <MdOutlineMailOutline size={30}></MdOutlineMailOutline> </a>
-                            <a href="https://www.linkedin.com/in/john-philip-baylon-0b90b0373/"><FaLinkedin size={30} /></a>
+                            <a href="https://www.facebook.com/johnphilip.baylon.5"><FaFacebook></FaFacebook></a>
+                            <a href="mailto:johnphilipbaylon3@gmail.com"> <MdOutlineMailOutline ></MdOutlineMailOutline> </a>
+                            <a href="https://www.linkedin.com/in/john-philip-baylon-0b90b0373/"><FaLinkedin  /></a>
                         </td>
                         <td>
                             <div>Jump Jump</div>
